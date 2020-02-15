c06efdadbebc3c5cd5ba460d2b771caab2df2dca
Dave Irwin <david@redirwin.com>
2020-02-14 22:31:31 -0700

    Part 2 completed.

--- Assignment.java
+++ Assignment.java
@@ -3,7 +3,14 @@ public class Assignment {
 
     public static void main(String[] args) {
 
-        // Part 1.	Create an array of int called ages that contains the 
+    //    ages();     
+       names();   
+        
+    }
+
+    public static void ages() {
+
+         // Part 1:	Create an array of int called ages that contains the 
         // following values: 3, 9, 23, 64, 2, 8, 28, 93.
         
             // a. Programmatically subtract the value of the first element 
@@ -31,7 +38,39 @@ public class Assignment {
         }
 
         System.out.println(ageSum / ages.length);       
-        
+
+    }
+    
+    public static void names() {
+
+        // Part 2: Create an array of String called names that contains the following values: 
+        // “Sam”, “Tommy”, “Tim”, “Sally”, “Buck”, “Bob”.
+            // a.	Use a loop to iterate through the array and calculate the average number 
+            // of letters per name. Print the result to the console.
+            // b.	Use a loop to iterate through the array again and concatenate all the names 
+            // together, separated by spaces, and print the result to the console.
+
+        String[] names = new String[]{"Tommy", "Tim", "Sally", "Buck", "Bob"};
+
+        int charCount = 0; 
+
+        for (String name : names) {     
+            charCount += name.length(); 
+        }
+
+        System.out.println("Average number of letters: " + (charCount / names.length)); 
+
+        String allNames = "";
+
+        for (String name: names) {
+            allNames = allNames.concat(name).concat(" ");
+        }
+
+        System.out.println("All names: " + allNames);
+
         
     }
+
+
+
 }
