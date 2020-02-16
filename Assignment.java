677973ca35b17f1d96d248d2c6548d63f9efe4d7
Dave Irwin <david@redirwin.com>
2020-02-15 18:24:43 -0700

    Answered parts 3 & 4.

--- Assignment.java
+++ Assignment.java
@@ -3,7 +3,7 @@ public class Assignment {
 
     public static void main(String[] args) {
 
-    //    ages();     
+       ages();     
        names();   
         
     }
@@ -12,23 +12,25 @@ public class Assignment {
 
          // Part 1:	Create an array of int called ages that contains the 
         // following values: 3, 9, 23, 64, 2, 8, 28, 93.
-        
-            // a. Programmatically subtract the value of the first element 
-            // in the array from the value in the last element of the array 
-            // (do not type ANY numbers in the operation, ages[7] – ages[0] 
-            // is not allowed). Print the result to the console.
-            
-            // b. Add a new age to your array and repeat the step above to 
-            // ensure it is dynamic (works for arrays of different lengths).
-            
-            // c. Use a loop to iterate through the array and calculate the 
-            // average age. Print the result to the console.
-        
+
         int[] ages = new int[]{3, 9, 23, 64, 2, 8, 28, 93, 41};
+    
+        // a. Programmatically subtract the value of the first element 
+        // in the array from the value in the last element of the array 
+        // (do not type ANY numbers in the operation, ages[7] – ages[0] 
+        // is not allowed). Print the result to the console.
 
         int first = ages[ages.length - ages.length]; // can't input [0], so calculate instead
         int last = ages[ages.length - 1]; // last item is at (length - 1) since arrays start at 0
+        
+        // b. Add a new age to your array and repeat the step above to 
+        // ensure it is dynamic (works for arrays of different lengths).
 
+            // SEE ARRAY ABOVE
+        
+        // c. Use a loop to iterate through the array and calculate the 
+        // average age. Print the result to the console.
+        
         System.out.println(last - first);
 
         double ageSum = 0;
@@ -37,7 +39,7 @@ public class Assignment {
             ageSum += ages[i];
         }
 
-        System.out.println(ageSum / ages.length);       
+        System.out.println(ageSum / ages.length);  
 
     }
     
@@ -45,12 +47,11 @@ public class Assignment {
 
         // Part 2: Create an array of String called names that contains the following values: 
         // “Sam”, “Tommy”, “Tim”, “Sally”, “Buck”, “Bob”.
-            // a.	Use a loop to iterate through the array and calculate the average number 
-            // of letters per name. Print the result to the console.
-            // b.	Use a loop to iterate through the array again and concatenate all the names 
-            // together, separated by spaces, and print the result to the console.
 
-        String[] names = new String[]{"Tommy", "Tim", "Sally", "Buck", "Bob"};
+        String[] names = new String[]{"Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"};
+
+        // a.	Use a loop to iterate through the array and calculate the average number 
+        // of letters per name. Print the result to the console.
 
         int charCount = 0; 
 
@@ -60,6 +61,9 @@ public class Assignment {
 
         System.out.println("Average number of letters: " + (charCount / names.length)); 
 
+        // b.	Use a loop to iterate through the array again and concatenate all the names 
+        // together, separated by spaces, and print the result to the console.
+
         String allNames = "";
 
         for (String name: names) {
@@ -67,7 +71,6 @@ public class Assignment {
         }
 
         System.out.println("All names: " + allNames);
-
         
     }
 
